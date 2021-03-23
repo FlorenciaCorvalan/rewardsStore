c973c7ee1428db556961cf5debc9659117978e67
Florencia corvalan <florcorvalan26@gmail.com>
2021-03-22 22:24:31 -0300

    check

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -2,6 +2,4 @@ import React, { useContext } from "react";
 import { userContext } from "../Context/UserContext";
 import styled from "styled-components";
 
-const Filter () => {
-    const { productData, setCategory} = useContext(userContext);
-}
+
--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from "react";
+import { userContext } from "../Context/UserContext";
 import styled from "styled-components";
 
 const CardContainer = styled.div`
