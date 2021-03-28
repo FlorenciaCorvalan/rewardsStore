9534e7e4d89f5c62cf65aca483f8116a7d1bc3b2
Florencia corvalan <florcorvalan26@gmail.com>
2021-03-28 14:43:37 -0300

    Filter and other changes

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,5 +1,30 @@
 import React, { useContext } from "react";
-import { userContext } from "../Context/UserContext";
+import { UserContext } from "../Context/UserContext";
 import styled from "styled-components";
+
+export const Filter = () => {
+    const { productData, setCategory } = useContext(UserContext);
+    return(
+        <>
+        <div>
+            <div>
+                <label>Filter by category</label>
+                <select name="category" value={productData.category} onChange={(e) =>{
+                    const value = e.target.value;
+                    setCategory({ ...productData, [e.target.name]: value});
+                }}>
+                    <option value="">Select category</option>
+                    {[...new Set(productData.map((e) => e.category))]
+                    .sort()
+                    .map((item, index) =>(
+                        <option key={`${item}-${index}`} value={item}>{item}</option>
+                    ))}
+                </select>
+            </div>
+            <hr />
+        </div>
+        </>
+    );
+}
 
 
